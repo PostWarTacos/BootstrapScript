defd2b9b69daf63e6be76a60d92bc010f059d8e3
PostWarTacos <mwurtz.soc@gmail.com>
2025-06-09 14:14:51 -0500

    update

--- obj/Debug/net9.0/BootstrapScript.AssemblyInfo.cs
+++ obj/Debug/net9.0/BootstrapScript.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BootstrapScript")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8336c0668e5522745cfa14bb75ddefbb530f0cae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1f80f311c6926363ba7cc78eb83871749f98f10")]
 [assembly: System.Reflection.AssemblyProductAttribute("BootstrapScript")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BootstrapScript")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
